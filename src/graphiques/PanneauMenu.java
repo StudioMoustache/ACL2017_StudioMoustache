0fa5622f928416685eb3f6b6833600c38d15587a
Frodonche <guiguix3@hotmail.fr>
2017-11-30 16:01:12 +0100

    Images du menu (titre + 1P et 2P)

--- src/graphiques/PanneauMenu.java
+++ src/graphiques/PanneauMenu.java
@@ -3,11 +3,17 @@ package graphiques;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.event.KeyListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.awt.event.KeyEvent;
 
 import java.awt.GridLayout;
+import java.awt.Image;
 
 import javax.swing.JPanel;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
 import javax.swing.SwingConstants;
@@ -25,8 +31,23 @@ public class PanneauMenu extends JPanel {
 		// Layout sous forme de grille de 3 lignes et de 1 colonne
 		setLayout(new GridLayout(2, 1));
 
-		// Création du JLabel de titre de jeu
-		JLabel titre = new JLabel("BERZERKER");
+		// Importation des images du menu
+		Image menuImg = null;
+		Image onePlayerT = null;
+		Image twoPlayerT = null;
+		try {
+			menuImg = ImageIO.read(new File("src/images/menu.jpg"));
+			BufferedImage onePlayer = ImageIO.read(new File("src/images/1_players.png"));
+			onePlayerT =  onePlayer.getScaledInstance(245, 245, onePlayer.SCALE_DEFAULT);
+			BufferedImage twoPlayer = ImageIO.read(new File("src/images/2_players.png"));
+			twoPlayerT =  twoPlayer.getScaledInstance(245, 245, twoPlayer.SCALE_DEFAULT);
+		} catch (IOException e1) {
+			e1.printStackTrace();
+		}
+		
+		
+		// Creation du JLabel de titre de jeu
+		JLabel titre = new JLabel(new ImageIcon(menuImg));
 		titre.setHorizontalAlignment(SwingConstants.CENTER);
 		titre.setVerticalAlignment(SwingConstants.CENTER);
 
@@ -39,8 +60,11 @@ public class PanneauMenu extends JPanel {
 		// Layout sous forme de grille de 1 ligne et de 2 colonnes
 		choixNbJoueurs.setLayout(new GridLayout(1, 2));
 
+		
+		
 		// Creation du JButton de choix 1 joueur
-		JButton unJoueur = new JButton("1 joueur");
+		
+		JButton unJoueur = new JButton(new ImageIcon(onePlayerT));
 		unJoueur.setHorizontalAlignment(SwingConstants.CENTER);
 		unJoueur.setVerticalAlignment(SwingConstants.CENTER);
 		unJoueur.addActionListener(new ActionListener() {
@@ -54,7 +78,7 @@ public class PanneauMenu extends JPanel {
 		choixNbJoueurs.add(unJoueur);
 
 		// Creation du JButton de choix 1 joueur
-		JButton deuxJoueurs = new JButton("2 joueurs");
+		JButton deuxJoueurs = new JButton(new ImageIcon(twoPlayerT));
 		deuxJoueurs.setHorizontalAlignment(SwingConstants.CENTER);
 		deuxJoueurs.setVerticalAlignment(SwingConstants.CENTER);
 		deuxJoueurs.addActionListener(new ActionListener() {
